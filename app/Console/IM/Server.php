bba9494e80976fef97eb3d9822ee8266eef37289
huyibin <804141892@qq.com>
2020-05-14 20:55:53 +0800

    save

--- app/Console/IM/Server.php
+++ app/Console/IM/Server.php
@@ -8,16 +8,12 @@ class Server
 {
     protected $server;
 
-    protected $events = [
-        'open' => null,
-        'message' => null,
-        'close' => null,
-    ];
+
 
     public function __construct($host = '0.0.0.0',$port = 9502,array $callbacks = null)
     {
         $this->server = new \Swoole\WebSocket\Server($host,$port);
-        throw_if($callbacks,\Exception::class,'回调函数不能为空');
+        throw_if(empty($callbacks),\Exception::class,'回调函数不能为空');
         $this->setListener($callbacks);
     }
 
@@ -30,10 +26,7 @@ class Server
 
     protected function setListener($callbacks){
         foreach($callbacks as $event => $callback ){
-            if(isset($this->events[$event])){
-                $this->events[$event] = $callback;
-                $this->server->on($event,$callback);
-            }
+            $this->server->on($event,$callback);
         }
     }
 }
