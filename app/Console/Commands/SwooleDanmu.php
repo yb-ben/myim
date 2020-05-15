d1e4babfb32ad327a255c4b0c941b6e9d049041e
huyibin <804141892@qq.com>
2020-05-15 17:51:42 +0800

    save

---
+++ app/Console/Commands/SwooleDanmu.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Swoole\Coroutine\Http\Client;
+use Swoole\WebSocket\Server;
+
+class SwooleDanmu extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'swoole:danmu';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'swoole websocket';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        go(function(){
+            $roomId = '21664698';
+            $client = new Client('api.live.bilibili.com',80,true);
+            $client->setHeaders([
+                'Host'=>'api.live.bilibili.com',
+                'origin'=>'https://live.bilibili.com',
+                'referer'=>'https://live.bilibili.com/'.$roomId,
+                'accept'=>'application/json, text/javascript, */*; q=0.01',
+                'accept-encoding'=>'gzip, deflate, br',
+                'accept-language'=>'zh-CN,zh;q=0.9',
+                'user-agent'=>'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+            ]);
+            $client->set(['timeout'=>3]);
+            $client->get("/room/v1/Danmu/getConf?room_id=${roomId}&platform=pc&player=web");
+            echo $client->body;
+            $client->close();
+        });
+    }
+}
