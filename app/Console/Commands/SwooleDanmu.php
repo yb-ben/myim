bd0020a66f1cf41702f4d0774fe5bc53632b6d7e
huyibin <804141892@qq.com>
2020-05-15 19:39:41 +0800

    save

--- app/Console/Commands/SwooleDanmu.php
+++ app/Console/Commands/SwooleDanmu.php
@@ -53,8 +53,49 @@ class SwooleDanmu extends Command
             ]);
             $client->set(['timeout'=>3]);
             $client->get("/room/v1/Danmu/getConf?room_id=${roomId}&platform=pc&player=web");
-            echo $client->body;
+            $body = json_decode($client->body,JSON_OBJECT_AS_ARRAY);
+            print_r($body);
             $client->close();
+            if($body['code'] === 0 ){
+                $server = $body['data']['host_server_list'][0];
+                $key = $body['data']['token'];
+                $first = [
+                    'uid'=>'4906232',
+                    'roomId' => $roomId,
+                    'protover'=>2,
+                    "platform"=>"web",
+                    "clientver"=>"1.12.0",
+                    "type"=>2,
+                    'token'=>$key
+                ];
+
+                $heartBit = hex2bin('0000001f0010000100000002000000015b6f626a656374204f626a6563745d');
+
+                $payload= bin2hex(json_encode($first));
+
+                $payload = '0000'.dechex(strlen($payload) + 32).'001000010000000700000001'.$payload;
+
+
+                $ws =  new Client($server['host'],$server['wss_port'],true);
+                $ret = $ws->upgrade('/sub');
+                if($ret){
+
+                    $ws->push(hex2bin($payload),WEBSOCKET_OPCODE_BINARY);
+                    go(function ()use($ws){
+                        while(true){
+                            $msg = $ws->recv(3);
+                            var_dump($msg);
+                        }
+                    });
+                    go(function ()use($ws,$heartBit){
+                       while(true){
+                           $ws->push($heartBit,WEBSOCKET_OPCODE_BINARY);
+                           var_dump(1);
+                           sleep(1);
+                       }
+                    });
+                }
+            }
         });
     }
 }
