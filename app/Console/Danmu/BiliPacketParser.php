d4aa4b0a64943713f0869af20cfb3499d7bdd6fe
huyibin <804141892@qq.com>
2020-05-17 16:15:03 +0800

    save

--- app/Console/Danmu/BiliPacketParser.php
+++ app/Console/Danmu/BiliPacketParser.php
@@ -70,7 +70,7 @@ class BiliPacketParser
 
         if($d['cmd'] === 'DANMU_MSG'){
             $uid = $d['info'][2][0];
-            if($uid === 421267475){
+            if($uid === 421267475 || false !== mb_strpos($d['info'][1],'艾因')){
                 $this->buffer[] = [$d['info'][1],$uid,$d['info'][2][1],date('Y-m-d H:i:s',$d['info'][9]['ts'])];
             }
         }
